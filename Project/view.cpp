20cb4273605936f94e1c311f9b3feb959061feab
wjn <1552443053@qq.com>
2021-04-03 17:59:36 +0800

    create solution

---
+++ Project/view.cpp
@@ -0,0 +1,7 @@
+#include "view.h"
+
+View::View(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
---
+++ Project/view.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_view.h"
+
+class View : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    View(QWidget *parent = Q_NULLPTR);
+
+private:
+    Ui::ViewClass ui;
+};
